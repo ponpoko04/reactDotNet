33d1e4801c1cbc214ce986c528bced9be911be24
takayoshi.uchida <taum0715@gmail.com>
2016-07-03 01:10:32 +0900

    First Commit. React.js with ASP.NET Web API

---
+++ ReactDotNetApi/Controllers/CommentsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ReactDotNetApi.Models;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ReactDotNetApi.Controllers
+{
+    public class CommentsController : ApiController
+    {
+        List<Comment> comments = new List<Comment>
+        {
+            new Comment { id = 1, author = "Pete Hunt", text = "This is one comment" },
+            new Comment { id = 2, author = "Jordan Walke", text = "This is *author* comment" }
+        };
+
+        public IEnumerable<Comment> GetAllComments()
+        {
+            return this.comments;
+        }
+
+        public IHttpActionResult GetComment(int id)
+        {
+            var comment = this.comments.FirstOrDefault((p) => p.id == id);
+            if (comment == null) return NotFound();
+            return Ok(comment);
+        }
+
+        public IHttpActionResult Post([FromBody]JToken json)
+        {
+            if (json == null) return Ok(this.comments);
+            var postComment = new Comment()
+            {
+                id = this.comments.Max((p) => p.id) + 1,
+                author = json["author"].ToString(),
+                text = json["text"].ToString()
+            };
+            this.comments.Add(postComment);
+            return Ok(this.comments);
+        }
+    }
+}
---
+++ ReactDotNetApi/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ReactDotNetApi.Models
+{
+    [DataContract]
+    public class Comment
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string author { get; set; }
+        [DataMember]
+        public string text { get; set; }
+    }
+}
